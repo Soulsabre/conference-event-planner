7c7cf63ecebea40e3fbb786b7cf4d395809daa28
Schawn Statham <sstatham.dev@outlook.com>
2025-02-23 16:36:16 -0500

    Finished coding for first version of the project

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
 import avReducer from './avSlice';
-import mealsReducers from '.mealsSlice';
+import mealsReducer from './mealsSlice';
 
 export default configureStore({
   reducer: {
